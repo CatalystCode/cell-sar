1cddd1ee0e62aed18eacd01ad2b8e2f5ced95198
Anthony Turner <anthony@bitoblivion.com>
2017-07-28 11:15:27 -0700

    Add scripts to manage the patch tree around Yate/YBTS/bladeRF

---
+++ devops/generatePatchedTree.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+echo "Generating patched source code trees..."
+echo "  Where am I?"
+
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+SCRIPTPATH=$(readlink -m "${SCRIPTPATH}")
+
+SANDBOXPATH=$(readlink -m "${SCRIPTPATH}/../.sandbox")
+LIBPATH=$(readlink -m "${SCRIPTPATH}/../lib")
+PATCHPATH=$(readlink -m "${SCRIPTPATH}/../bts")
+
+echo "      I'm at ${SCRIPTPATH}"
+if [ ! -d "${LIBPATH}" ]; then
+    echo "      Error: Libraries not in ${LIBPATH}"
+    exit 1;
+else
+    echo "      Original libraries are at ${LIBPATH}"
+fi
+
+if [ ! -d "${PATCHPATH}" ]; then
+    echo "      Error: Patches not in ${PATCHPATH}"
+    exit 1;
+else
+    echo "      Patches are at ${PATCHPATH}"
+fi
+
+echo "  Creating sandbox at ${SANDBOXPATH}"
+
+rm -rf "${SANDBOXPATH}"
+mkdir -p "${SANDBOXPATH}"
+
+cp -R "${LIBPATH}"/* "${SANDBOXPATH}"
+
+echo "Applying patches..."
+# Iterate over libraries
+shopt -s nullglob
+for libDirectory in `ls -d ${LIBPATH}/* `; do
+    echo "  Applying patches to ${libDirectory}..."
+    for patchFile in "${libDirectory}/*.patch"; do
+        echo "  Processing ${patchFile}...";
+        cd "${libDirectory}/"
+        # patch < ${patchFile}
+        # patch -b < ${patchFile}        
+    done
+done
+
+echo "Done!"
